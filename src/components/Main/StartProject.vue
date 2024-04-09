d7b0b84aae5e0de1f98a4493da41d3c260fce93f
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-04-09 17:26:50 +0200

    add start project style

--- src/components/Main/StartProject.vue
+++ src/components/Main/StartProject.vue
@@ -20,39 +20,16 @@ export default {
 
 <template>
     <div id="start_project">
-        <div class="left">
-            <img src="/public/images/nemoodar.png" alt="">
-            <img class="top_image" src="/public/images/men-data.png" alt="">
-        </div>
-        <div class="right">
-            <BlueCard :card="card" />
+        <div class="container">
+            <div class="left">
+                <img src="/public/images/nemoodar.png" alt="">
+                <img class="top_image" src="/public/images/men-data.png" alt="">
+            </div>
+            <div class="right">
+                <BlueCard :card="card" />
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-#start_project {
-    background-color: paleturquoise;
-    display: flex;
-    justify-content: space-between;
-
-    .left {
-        width: 200px;
-        position: relative;
-
-        img {
-            width: 300px;
-        }
-
-        .top_image {
-            position: absolute;
-            top: 30px;
-            left: 35px;
-        }
-    }
-
-    .right {
-        width: 200px;
-    }
-}
-</style>
+<style scoped></style>
